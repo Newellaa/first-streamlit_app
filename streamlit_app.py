ebe1f2b3fd2ae8760c89cd463499714b8574ef2e
Newellaa <145040874+Newellaa@users.noreply.github.com>
2023-09-14 12:36:00 -0400

    Picker description Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,11 @@ streamlit.header('Build Your Own Fruit Smoothie')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list  here to pick the fruit
 streamlit.multiselect("Pick some Fruits", list(my_fruit_list.index))
 
 #Display Table
+streamlit.dataframe(my_fruit_list)
