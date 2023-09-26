efa81bcd0a9f309888b417b9bd7fd2dbccb7e22e
Newellaa <145040874+Newellaa@users.noreply.github.com>
2023-09-26 17:31:54 -0400

    Button

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,18 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
+#Snowflake related functions
+def get_fruit_load_list():
+    with my_cur = my_cnx.cursor() as my_cur:
+         my_cur.execute("SELECT * from fruit_load_list")
+         return my_cur.fetchall()
+
+#Add Button
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+if streamlit.button('get_fruit_load_list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 streamlit.title('Breakfast Favorites')
 
